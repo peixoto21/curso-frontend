9f2b76d275eefb8905abd68020c29e5934b0c6f0
Victor Peixoto <peixoto2112@gmail.com>
2022-04-22 19:30:34 -0300

    tirando conteudo a mais

--- gulp/projeto/gulpfile.js
+++ gulp/projeto/gulpfile.js
@@ -6,25 +6,40 @@ const uglify = require('gulp-uglify')
 const rename = require('gulp-rename')
 const image = require('gulp-image')
 
-function tarefasCSS(cb){
+function tarefasCSS(cb) {
 
-    return gulp.src('./vendor/**/*.css')
-        .pipe(concat('libs.css'))
-        .pipe(cssmin())
-        .pipe(rename({suffix:".min"}))
-        .pipe(gulp.dest('./dist/css'))
-    
+    return gulp.src([
+            './node_modules/bootstrap/dist/css/bootstrap.css',
+            './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+            './vendor/owl/css/owl.css',
+            './vendor/jquery-ui/jquery-ui.css',
+            './src/css/style.css'
+        ])
+        .pipe(stripCss())                   // remove comentários
+        .pipe(concat('styles.css'))         // mescla arquivos
+        .pipe(cssmin())                     // minifica css
+        .pipe(rename({ suffix: '.min'}))    // styles.min.css
+        .pipe(gulp.dest('./dist/css'))      // cria arquivo em novo diretório
 
 }
 
 function tarefasJS(){
 
-    return gulp.src('./vendor/**/*.js')
-        .pipe(concat('libs.js'))
-        .pipe(uglify())
-        .pipe(rename({suffix:".min"}))
-        .pipe(gulp.dest('./dist/js'))
+    return gulp.src([
+            './node_modules/jquery/dist/jquery.js',
+            './node_modules/bootstrap/dist/js/bootstrap.js',
+            './vendor/owl/js/owl.js',
+            './vendor/jquery-mask/jquery.mask.js',
+            './vendor/jquery-ui/jquery-ui.js',
+            './src/js/custom.js'
+        ])
+        .pipe(stripJs())                    // remove comentários
+        .pipe(concat('scripts.js'))         // mescla arquivos
+        .pipe(uglify())                     // minifica js
+        .pipe(rename({ suffix: '.min'}))    // scripts.min.js
+        .pipe(gulp.dest('./dist/js'))       // cria arquivo em novo diretório
 }
+
 
 function tarefasImagem(){
     
