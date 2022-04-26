d76663934b974114f61bd43a0281a006f16f3e58
Victor Peixoto <peixoto2112@gmail.com>
2022-04-26 16:01:29 -0300

    fix carousel, modificação identificação dos cards

--- jquery/js/custom.js
+++ jquery/js/custom.js
@@ -2,7 +2,7 @@
 // jQuery( function($){
 $(document).ready(function () {
 
-  $('.owl-carousel').owlCarousel();
+  // $('.owl-carousel').owlCarousel();
 
   let titulos = $('h4') // tag
 
@@ -18,37 +18,37 @@ $(document).ready(function () {
 
   //  $('.featured-item:first h4').append('<span class="position-relative top-0 start-new translate-middle badge bg-secondary" style="font-size: 0.6rem;">Novo</span>')
 
-  jQuery(function ($) {
-    var lastWidth = null;
-    $(window).bind('resize', function (e) {
-      if ((lastWidth == null || lastWidth < 1400) && window.innerWidth >= 1400) {
-        $('#novo').remove()
-        $('.featured-item:first h4').append('<span id="novo" class="position-relative top-0 start-5 translate-middle badge bg-secondary" style="font-size: 0.6rem;">Novo</span>')
-      }
-      else if ((lastWidth == null || lastWidth < 1200) && window.innerWidth >= 1200) {
-        $('#novo').remove()
-        $('.featured-item:first h4').append('<span id="novo"  class="position-relative top-0 start-6 translate-middle badge bg-secondary" style="font-size: 0.6rem;">Novo</span>')
-      }
+  // jQuery(function ($) {
+  //   var lastWidth = null;
+  //   $(window).bind('resize', function (e) {
+  //     if ((lastWidth == null || lastWidth < 1400) && window.innerWidth >= 1400) {
+  //       $('#novo').remove()
+  //       $('.featured-item:first h4').append('<span id="novo" class="position-relative top-0 start-5 translate-middle badge bg-secondary" style="font-size: 0.6rem;">Novo</span>')
+  //     }
+  //     else if ((lastWidth == null || lastWidth < 1200) && window.innerWidth >= 1200) {
+  //       $('#novo').remove()
+  //       $('.featured-item:first h4').append('<span id="novo"  class="position-relative top-0 start-6 translate-middle badge bg-secondary" style="font-size: 0.6rem;">Novo</span>')
+  //     }
 
-      else if ((lastWidth == null || lastWidth < 991) && window.innerWidth >= 991) {
-        $('#novo').remove()
-        $('.featured-item:first h4').append('<span id="novo"  class="position-relative top-0 start-7 translate-middle badge bg-secondary" style="font-size: 0.6rem;">Novo</span>')
-      }
+  //     else if ((lastWidth == null || lastWidth < 991) && window.innerWidth >= 991) {
+  //       $('#novo').remove()
+  //       $('.featured-item:first h4').append('<span id="novo"  class="position-relative top-0 start-7 translate-middle badge bg-secondary" style="font-size: 0.6rem;">Novo</span>')
+  //     }
 
-      else if ((lastWidth == null || lastWidth < 768) && window.innerWidth >= 768) {
-        $('#novo').remove()
-        $('.featured-item:first h4').append('<span id="novo"  class="position-relative top-0 start-9 translate-middle badge bg-secondary" style="font-size: 0.6rem;">Novo</span>')
-      }
+  //     else if ((lastWidth == null || lastWidth < 768) && window.innerWidth >= 768) {
+  //       $('#novo').remove()
+  //       $('.featured-item:first h4').append('<span id="novo"  class="position-relative top-0 start-9 translate-middle badge bg-secondary" style="font-size: 0.6rem;">Novo</span>')
+  //     }
 
-      else if ((lastWidth == null || lastWidth > 425) && window.innerWidth <= 425) {
-        $('#novo').remove()
-        $('.featured-item:first h4').append('<span id="novo"  class="position-relative top-0 start-100 translate-middle badge bg-secondary" style="font-size: 0.5rem;">Novo</span>')
-      }
+  //     else if ((lastWidth == null || lastWidth > 425) && window.innerWidth <= 425) {
+  //       $('#novo').remove()
+  //       $('.featured-item:first h4').append('<span id="novo"  class="position-relative top-0 start-100 translate-middle badge bg-secondary" style="font-size: 0.5rem;">Novo</span>')
+  //     }
 
 
-      lastWidth = window.innerWidth;
-    }).trigger('resize');
-  });
+  //     lastWidth = window.innerWidth;
+  //   }).trigger('resize');
+  // });
   // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
   // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
   // $('.featured-item:first h4').addClass('active')
@@ -73,7 +73,7 @@ $(document).ready(function () {
   /*
     * Manipulação de eventos
     */
-  $('.featured-item a').on('click', function (event) {
+  $('#emfalta').on('click', function (event) {
 
     event.preventDefault();
 
@@ -81,7 +81,32 @@ $(document).ready(function () {
 
   })
 
+  $('#estoque').on('click', function (event) {
 
+    event.preventDefault();
+    alert('Produto em estoque!')
+
+  })
+
+  $('.owl-carousel').owlCarousel({
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
   /* 
     * Callback
     * entendendo ações que começam ao termino de outra
